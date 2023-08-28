b04f6d41fb6428f7aab6a70058a7229fb1c2e793
kyalo3 <kyalokimeu0@gmail.com>
2023-08-28 18:52:15 +0300

    Write a function that prints an integer with an error message

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
 
 def safe_print_integer_err(value):
-    print_elements = 0
-    x = print_elements
-    for i in range(x):
-        try:
-            print("{:d}".format(my_list[i]))
-        except (ValueError, TypeError):
-            pass
-        else:
-            print_elements += 1
-    print()
-    return (print_elements)
+    import sys
+    try:
+        print("{:d}".format(value))
+    except Exception as i:
+        sys.stderr.write("Exception: {}\n".format(i))
+        return False
+    else:
+        return True
