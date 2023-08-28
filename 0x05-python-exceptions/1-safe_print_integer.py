2fed4dcc4261ff1f4fd2586165802a69f85c339f
kyalo3 <kyalokimeu0@gmail.com>
2023-08-28 14:38:11 +0300

    Write a function that prints an integer with {:d}.format().

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,13 +1,8 @@
 #!/usr/bin/python3
 
 def safe_print_integer(value):
-	value = 98
 	try:
-	if (value = 98)
 		print("{:d}".format(value), end = "")
 		return True
 	except:
-	else:
-		print("{:d}".format(value), end = "")
-
-	return False
+		return False
