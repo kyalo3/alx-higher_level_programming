d8e23e4369999eb0f3035f259e8235be5b85df8f
kyalo3 <kyalokimeu0@gmail.com>
2023-09-04 18:45:11 +0300

    Write a class Rectangle :Private instance attribute: width and height and finds the area and perimeter

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+"""
+class Rectangle that defines a rectangle by:
+    Private instance attribute: width
+    Private instance attribute: height
+"""
+
+
+class Rectangle:
+    """Class Rectangle """
+    def __init__(self, width=0, height=0):
+        """Instantiate class Rectangle: width and height"""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Get rectangle:width"""
+        return self.__width
+
+    @property
+    def height(self):
+        """Get rectangle:height"""
+        return self.__height
+
+    @width.setter
+    def width(self, value):
+        """set a new rectangle width"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @height.setter
+    def height(self, value):
+        """set a new rectangle height"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """area of the reactangle"""
+        return 2 * (self.__width * self.__height)
+
+    def perimeter(self):
+        """perimeter of the rectangle"""
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return (self.__width + self.__height)
