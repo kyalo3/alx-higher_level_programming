847bc58c81278d40e9d9d56b25307b73c6806c3f
kyalo3 <kyalokimeu0@gmail.com>
2023-09-05 19:49:31 +0300

    Copy a list object

---
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -0,0 +1,4 @@
+def magic_string():
+    for i in range(10):
+    i += "BestSchool"
+    return(i)
--- 0x09-python-everything_is_object/19-copy_list.py
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -1,4 +1,3 @@
 def copy_list(l):
     my_list = [1, 2, 3]
-    l = my_list
-    return (l)
+    return (my_list)
