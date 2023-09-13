d4f8be3e7d4fd1279925858ee6c79bb121ff8414
kyalo3 <kyalokimeu0@gmail.com>
2023-09-13 12:50:35 +0300

    Write a class BaseGeometry -based on 6-base_geometry.py

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -17,7 +17,7 @@ class BaseGeometry:
             name (sring): a string
             value (int): the intager value
         """
-        if isinstance(value, int) and value > 0:
+        if not isinstance(value, int):
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
