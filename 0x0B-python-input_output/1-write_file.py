a4ceddb65f4e56b7e4e5b747569a4127c575db82
kyalo3 <kyalokimeu0@gmail.com>
2023-09-14 10:42:43 +0300

    1. Write to a file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
+"""
+a function that writes a string to a text file
+"""
+
 
 def write_file(filename="", text=""):
     """a function that reads a text file and prints it to stdout"""
 
-    with open(filename, "w", encoding="utf-8") as myFile:
-        myFile.write(text)
-
-    with open(filename, encoding="utf-8") as f:
-        read = f.read()
-        return len(read)
+    with open(filename, "w", encoding="utf-8") as f:
+        return f.write(text)
