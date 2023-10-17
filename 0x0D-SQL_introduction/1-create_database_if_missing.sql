161f883933300c44e6ddf10a40f0a63a9dd126ba
kyalo3 <kyalokimeu0@gmail.com>
2023-10-17 19:23:00 +0300

    1. Create a database

---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,12 @@
+-- SQL Script to Create Employee Table
+-- Author: Your Name
+-- Date: 2023-10-03
+
+CREATE TABLE Customer(
+    CustomerID INT PRIMARY KEY,
+    CustomerName VARCHAR(50),
+    LastName VARCHAR(50),
+    Country VARCHAR(50),
+    Age int(2),
+  Phone int(10)
+);
