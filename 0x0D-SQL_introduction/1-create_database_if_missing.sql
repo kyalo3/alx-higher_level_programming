447db24a5198c897ba75ea2c5c3935ff068e848b
kyalo3 <kyalokimeu0@gmail.com>
2023-10-17 20:02:04 +0300

    create database

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -4,10 +4,5 @@
 
 SHOW DATABASES;
 CREATE DATABASE<hbtn_0c_0>(
-    CustomerID INT PRIMARY KEY,
-    CustomerName VARCHAR(50),
-    LastName VARCHAR(50),
-    Country VARCHAR(50),
-    Age int(2),
-  Phone int(10)
+		
 );
