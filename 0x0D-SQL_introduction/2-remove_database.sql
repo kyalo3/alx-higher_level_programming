382b85fd7d9f3eb049cbc06a93ea3bf0e3609e16
kyalo3 <kyalokimeu0@gmail.com>
2023-10-17 18:27:58 -0700

    Delete a database

--- 0x0D-SQL_introduction/2-remove_database.sql
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -1,12 +1,2 @@
--- SQL Script to Create Employee Table
--- Author: Your Name
--- Date: 2023-10-03
-
-CREATE DATABASE<hbtn_0c_0>(
-    CustomerID INT PRIMARY KEY,
-    CustomerName VARCHAR(50),
-    LastName VARCHAR(50),
-    Country VARCHAR(50),
-    Age int(2),
-  Phone int(10)
-);
+-- SQL Script
+DROP DATABASE IF EXISTS hbtn_0c_0;
