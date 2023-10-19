ee038e03eb3278049a58c9b0331e34ca20eb11de
kyalo3 <kyalokimeu0@gmail.com>
2023-10-19 19:24:10 +0300

    a script that displays the top 3 of cities temperature during July and August ordered by temperature (descending)

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,10 +1,16 @@
--- script that displays the average temperature (Fahrenheit) by city order
+-- Import the table dump into hbtn_0c_0 database if not already imported
+-- You can use the source command in the MySQL command-line:
+-- SOURCE /path/to/table_dump.sql;
+
 SELECT
     city,
-    AVG(temperature) AS avg_temperature_fahrenheit
+    MAX(temperature) AS max_temperature
 FROM
-    temperatures -- Replace with the actual table name
+    temperatures
+WHERE
+    month IN (7, 8)  -- Filter for July and August
 GROUP BY
     city
 ORDER BY
-    avg_temperature_fahrenheit DESC;
+    max_temperature DESC
+LIMIT 3;
