1d0c43fc7be6e28e6ff076892adb0e79b013431f
kyalo3 <kyalokimeu0@gmail.com>
2023-11-17 10:21:53 +0300

    port number

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -16,7 +16,7 @@ if __name__ == "__main__":
     db = argv[3]
 
     """connection to the database"""
-    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+    db = MySQLdb.connect([port=3306, user=argv[1], passwd=argv[2], db=argv[3])
     cursor = db.cursor()
     cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
 
