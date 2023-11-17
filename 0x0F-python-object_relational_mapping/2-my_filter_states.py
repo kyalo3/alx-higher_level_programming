0a4f22be920287c6faa6eacf6be7f4b8e9f3f06d
kyalo3 <kyalokimeu0@gmail.com>
2023-11-17 08:16:38 +0300

    2. Filter states by user input Write a script that takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument.

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    "command line arguments"
+    user = argv[1]
+    passwd = argv[2]
+    db = argv[3]
+    state = argv[4]
+
+    "connection to my database"
+    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+    cursor = db.cursor()
+
+    query = ("SELECT * FROM states "
+             "WHERE name LIKE BINARY '{}' "
+             "ORDER BY states.id ASC".format(state))
+    cursor.execute(query)
+
+    "fetch all the rows"
+    rows = cursor.fetchall()
+
+    for row in rows:
+        print(row)
+
+    "close the cursor nd connection"
+    cursor.close()
+    db.close()
