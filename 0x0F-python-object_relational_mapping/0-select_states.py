a4a9e6c0313a6dd0c1657fa5b4f0d015e76c147d
kyalo3 <kyalokimeu0@gmail.com>
2023-11-17 09:40:23 +0300

    documentation

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,20 +3,26 @@
 import MySQLdb
 from sys import argv
 
+"""arguments passed as parameters"""
 user = argv[1]
 passwd = argv[2]
 db = argv[3]
 
 if __name__ == "__main__":
-    "command line arguments"
+    """command line arguments"""
     db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+
+    """getting cursor and execution"""
     cursor = db.cursor()
     cursor.execute("SELECT * FROM states")
 
+    """print results in delimited format"""
     rows = cursor.fetchall()
 
+    """iterate the rows"""
     for row in rows:
         print(row)
 
+    """close both dtabse and cursor"""
     cursor.close()
     db.close()
