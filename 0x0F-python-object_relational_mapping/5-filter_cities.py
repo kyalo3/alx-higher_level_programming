c00061d944b757a021c86f8ba8e93d137d71b67d
kyalo3 <kyalokimeu0@gmail.com>
2023-11-17 14:56:36 +0300

    5. All cities by state Write a script that takes in the name of a state as an argument and lists all cities of that state, using the database hbtn_0e_4_usa

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+
+"""
+a script that takes in the name of a state as an
+argumenta and lists all cities of that state
+using the database hbtn_0e_4_usa
+"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    user = argv[1]
+    passwd = argv[2]
+    db = argv[3]
+    state_name = argv[4]
+
+    """command line arguments"""
+    db = MySQLdb.connect(port=3306,	user=argv[1], passwd=argv[2], db=argv[3])
+
+    """getting cursor and execution"""
+    cursor = db.cursor()
+    query = """
+        SELECT cities.id, cities.name
+        FROM cities
+        JOIN states ON cities.state_id = states.id
+        WHERE states.name = %s
+        ORDER BY cities.id ASC
+    """
+
+    cursor.execute(query, (state_name,))
+
+    """fetch all the rows"""
+    rows = cursor.fetchall()
+    for city in rows:
+        print(city)
+
+    """close the cursor and database"""
+    cursor.close()
+    db.close()
