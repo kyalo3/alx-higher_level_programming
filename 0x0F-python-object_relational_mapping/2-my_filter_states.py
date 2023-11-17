a3297b8c1d17834e3165adba7b6034c561ed78b3
kyalo3 <kyalokimeu0@gmail.com>
2023-11-17 13:45:46 +0300

    task 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
 
+"""
+a script that takes in an argument and displays
+all values in the states table of hbtn_0e_0_usa
+where name matches the argument.
+"""
+
 import MySQLdb
 from sys import argv
 
@@ -11,7 +17,7 @@ if __name__ == "__main__":
     state = argv[4]
 
     "connection to my database"
-    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+    db = MySQLdb.connect(port=3306, user=argv[1], passwd=argv[2], db=argv[3])
     cursor = db.cursor()
 
     query = ("SELECT * FROM states "
@@ -25,6 +31,6 @@ if __name__ == "__main__":
     for row in rows:
         print(row)
 
-    "close the cursor nd connection"
+    "close the cursor and connection"
     cursor.close()
     db.close()
