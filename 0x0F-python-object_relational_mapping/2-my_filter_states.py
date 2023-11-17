f4913e5a38ac6b549fa006a4af297437bd300da2
kyalo3 <kyalokimeu0@gmail.com>
2023-11-17 13:40:22 +0300

    task 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,34 +1,30 @@
 #!/usr/bin/python3
 
-"""
-a script that displays all values in the
-states table of database hbtn_0e_0_usa
-where name matches the argument:
-"""
-
 import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
-    """command line arguments"""
+    "command line arguments"
     user = argv[1]
     passwd = argv[2]
     db = argv[3]
     state = argv[4]
 
-    """connection to the database"""
-    db = MySQLdb.connect(port=3306, user=argv[1], passwd=argv[2], db=argv[3])
-
-    query = "SELECT * FROM states WHERE  name LIKE BINARY %s ORDER BY id ASC"
+    "connection to my database"
+    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
     cursor = db.cursor()
-    cursor.execute(query, (argv[4],))
 
-    """fetch all rows"""
+    query = ("SELECT * FROM states "
+             "WHERE name LIKE BINARY '{}' "
+             "ORDER BY states.id ASC".format(state))
+    cursor.execute(query)
+
+    "fetch all the rows"
     rows = cursor.fetchall()
 
     for row in rows:
         print(row)
 
-    """close connections in cursor and db"""
+    "close the cursor nd connection"
     cursor.close()
     db.close()
