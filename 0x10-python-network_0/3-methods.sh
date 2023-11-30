90d1bcdcf0cb3be3ca7844705f301fc4d660f806
kyalo3 <kyalokimeu0@gmail.com>
2023-11-30 23:07:31 +0300

    3. cURL only methods

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 # bash script that takes in a URL and displays all methods
-methods=("GET" "HEAD" "POST" "PUT" "DELETE")
-for method in "${methods[@]}"; do
-   
-curl -X OPTIONS HEAD PUT "$1", "$2", "$3"
+curl -sI "$1" | grep "Allow" | cut -d " " -f 2-
--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -2,4 +2,4 @@
 # Bash script that takes in a URL as an argument
 #sends a GET request to the URL, and displays the body of the response
 url=$1
-curl -s -H "X-School-User-Id: 98" "url"
+curl -sL -H "X-School-User-Id: 98" "$1"
