37c38ff6354186e69bd9b4b26776e8ca9cb2dd15
kyalo3 <kyalokimeu0@gmail.com>
2023-12-01 13:27:49 +0300

    1. Response header value #0

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """a Python script that fetches"""
 from urllib.request import urlopen
+import sys
 
 url = "https://alx-intranet.hbtn.io"
 
 with urlopen(url) as response:
 
-    x_request_head = response.getheader("X-Request-Id")
-    print(x_request_head)
+    x_request_id = response.getheader("X-Request-Id")
+    print(x_request_id)
