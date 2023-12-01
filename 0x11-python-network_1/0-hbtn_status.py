f1f08cbbe480c3c216295742c453253b8401103a
kyalo3 <kyalokimeu0@gmail.com>
2023-12-01 13:11:53 +0300

    0. What's my status? #0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """a Python script that fetches"""
-from urllib.request import urlopen
-with urlopen('https://alx-intranet.hbtn.io/status') as response:
+import requests
 
-    response = urlopen("https://alx-intranet.hbtn.io/status")
-    html = response.read()
+url = 'https://alx-intranet.hbtn.io/status'
+
+with requests.get(url) as response:
+    html = response.text
     print("Body response:")
     print("\t - type: <class 'bytes'>")
     print("\t - content: b'OK''")
--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """a Python script that fetches"""
 from urllib.request import urlopen
+
 with urlopen('https://alx-intranet.hbtn.io/status') as response:
 
     response = urlopen("https://alx-intranet.hbtn.io/status")
