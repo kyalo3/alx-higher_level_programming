0456db8c439d87d07c4f8aa2ad41dee03649cd2e
Kyalo3 <kyalokimeu0@gmail.com>
2023-12-04 23:06:52 +0300

    new files

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,21 +1,18 @@
 #!/usr/bin/python3
-"""
-Python script that takes in a URL and an
-email, sends a POST request to the passed URL
-with the email as a parameter"""
+"""Sends a POST request to a given URL with a given email.
 
+Usage: ./2-post_email.py <URL> <email>
+  - Displays the body of the response.
+"""
 import sys
 import urllib.parse
 import urllib.request
 
 
 if __name__ == "__main__":
-    """ get URL and email from command line arguments"""
     url = sys.argv[1]
-    email = sys.argv[2]
-
-    """encode the email as a URL parameter"""
-    data = urllib.parse.urlencode({"email": sys.argv[2]}).encode("ascii")
+    value = {"email": sys.argv[2]}
+    data = urllib.parse.urlencode(value).encode("ascii")
 
     request = urllib.request.Request(url, data)
     with urllib.request.urlopen(request) as response:
