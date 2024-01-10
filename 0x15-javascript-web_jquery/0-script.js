e4b12fc78d01b1e3121b7755d7c230874cb2e43e
Kyalo3 <kyalokimeu0@gmail.com>
2024-01-11 00:46:05 +0300

    task 0

--- 0x15-javascript-web_jquery/0-script.js
+++ 0x15-javascript-web_jquery/0-script.js
@@ -1,7 +1,1 @@
-<script>
-    // Select the <header> element using document.querySelector
-    const headerElement = document.querySelector('header');
-
-    // Update the text color to red
-    headerElement.style.color = '#FF0000';
-</script>
+document.querySelector('HEADER').style.color = '#FF0000';
