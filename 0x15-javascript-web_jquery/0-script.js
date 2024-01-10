e8c8a5957fa5bc5193fc2c1bf8a205b56fe7b2af
Kyalo3 <kyalokimeu0@gmail.com>
2024-01-10 17:34:38 +0300

    0. No JQuery

---
+++ 0x15-javascript-web_jquery/0-script.js
@@ -0,0 +1,7 @@
+<script>
+    // Select the <header> element using document.querySelector
+    const headerElement = document.querySelector('header');
+
+    // Update the text color to red
+    headerElement.style.color = '#FF0000';
+</script>
