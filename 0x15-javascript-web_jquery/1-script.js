85561073223ab4c61c6b2838e427fd381051d313
Kyalo3 <kyalokimeu0@gmail.com>
2024-01-11 00:52:20 +0300

    task 1

--- 0x15-javascript-web_jquery/1-script.js
+++ 0x15-javascript-web_jquery/1-script.js
@@ -1,4 +1,1 @@
-$(document).ready(function() {
-    // Select the <header> element and update the text color to red
-    $('header').css('color', '#FF0000');
-});
+$('HEADER').css('color', '#FF0000');
